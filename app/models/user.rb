adfc1ec75ae493a43409609b8c2d411b40a92d05
Kazuki.M <kazuki0922m@gmail.com>
2022-07-27 22:47:05 +0900

    password_digestカラムの追加 バリデーションの追加

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password #password_dijest(パスワードのハッシュ化)
+
+  validates :password, presence: true,       #空白は許可しない
+                       length: {minimum: 6}  #最低６文字必要とする
+
+end
