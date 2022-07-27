bfe595af4901ed12c4fbbcab50f7ca28d18fa56a
Kazuki.M <kazuki0922m@gmail.com>
2022-07-28 00:52:33 +0900

    新規登録画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  root 'home#top'
+
+  get  "signup"  =>  "users#new"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
