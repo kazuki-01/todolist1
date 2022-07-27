064d745407654498adb287294d1392dec902054d
Kazuki.M <kazuki0922m@gmail.com>
2022-07-28 00:10:57 +0900

    バリデーションの追加（name,email）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_secure_password #password_dijest(パスワードのハッシュ化)
 
+  validates :name, presence: true
+  validates :email, presence: true
   validates :password, presence: true,       #空白は許可しない
                        length: {minimum: 6}  #最低６文字必要とする
+
+
 
 end
