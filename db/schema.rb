6a6155a381240b9281d1ac37e23d3781f8719a34
Kazuki.M <kazuki0922m@gmail.com>
2022-10-03 22:37:37 +0900

    doneカラムにオプションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_31_131807) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_132459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_131807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.boolean "done"
+    t.boolean "done", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
